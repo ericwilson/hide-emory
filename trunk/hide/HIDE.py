f2ad292068ccd3dd82e2165258bd73f8b3e40e66
pebbler <pebbler@2da577ee-07a7-11df-a26f-8b716316a5bc>
2010-03-23 12:12:57 +0000

    Added k-fold cross validation interface for testing CRF models.
    Added ability to use any trained CRF any set of documents based on tag.
    
    Removed all reliance on perl for conversion and feature generation. All of
    this code is now implemented in the HIDE.py module.
    
    Slightly updated the look of a couple of the templates.

--- trunk/hide/HIDE.py
+++ trunk/hide/HIDE.py
@@ -5,65 +5,26 @@ import urllib
 import subprocess
 import os
 import sys
+import time
+import xmlprinter
+import StringIO
 from elementtree import ElementTree
 from tempfile import NamedTemporaryFile
 
+from tidylib import tidy_document
 
 
 
-from xml.parsers import expat
+from JSAXParser import SGMLToMalletHandler
+
+
+from xml.sax import make_parser
+from xml.sax import parseString
+from xml.sax.handler import ContentHandler
+
 
 #these variables should be set in order for the HIDE module to work correctly
 #see loadConfig function.
-#HIDELIB
-#CONFIGTREE 
-
-class MalletParser:
-	def __init__(self):
-		self._parser = expat.ParserCreate()
-		self._parser.StartElementHandler = self.start
-		self._parser.EndElementHandler = self.end
-		self._parser.CharacterDataHandler = self.data
-		self.currentTag = "O"
-		self.output = ""
-	
-	def feed(self, data):
-		self._parser.Parse(data, 0)
-
-	def close(self):
-		self._parser.Parse("", 1) # end of data
-		del self._parser # get rid of circular references
-
-	def start(self, tag, attrs):
-		if (tag != 'object' and tag != 'br'):
-			self.currentTag = str(tag)
-		else:
-			self.currentTag = "O"
-	def end(self, tag):
-		self.currentTag = "O"
-        
-	def getOutput(self):
-		return self.output
-
-	def data(self, data):
-		currentTagCount = 0
-		p = re.compile('(\\W)')
-		vals = p.split(str(data))
-		for v in vals:
-			if ( v != '' ):
-				val = urllib.quote(v)
-				if ( self.currentTag != 'O'):
-					if ( currentTagCount == 0 ):
-						self.output += "TERM_" + val + "\tB-" + self.currentTag + "\n"
-					else:
-						self.output += "TERM_" + val + "\tI-" + self.currentTag + "\n"
-				else:
-					self.output += "TERM_" + val + "\tO" + "\n"
-				currentTagCount+=1
-
-	
-				
-							
 #Configuration
 def loadConfig( filename ):
    global CONFIGTREE
@@ -87,7 +48,7 @@ def loadConfig( filename ):
       if m:
          dictname = m.group(1)
          #open file and populate appropriate dictionary
-	 output += "loading " + dictname + " dictionary from " + fileName + "\n"
+	 #output += "loading " + dictname + " dictionary from " + fileName + "\n"
 	 DICTIONARY[dictname] = dict()
 	 f = open( DICTIONARYDIR + "/" + fileName, 'r' )
 	 while 1:
@@ -145,44 +106,88 @@ def doReplacement( sgml, repl, replvals ):
 	
         	
 def SGMLToMallet ( sgml ):
-	#convert the sgml to mallet
-	#p = MalletParser()
-	#p.feed(sgml)
-	#p.close()
-	#return p.getOutput()
-	#print "SGMLToMallet " + sgml
-	path= HIDELIB + "sgml2mallet-stdin.pl"
-	print >> sys.stderr, "[" + path + "]"
-	#add features to the mallet file
-	proc = subprocess.Popen("perl " + path,
-			shell=True,
-			stdin=subprocess.PIPE,
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE,
-			)
-	stdout_value, stderr_value = proc.communicate(str(sgml))
-	#print str(stderr_value)
-	#print "SGMLToMallet RETURNING " + stdout_value
-	return stdout_value
-	
-	
-	
+   xhtml, errors = tidy_document( sgml,
+      options={'numeric-entities':1, 'output-xml':1, 'add-xml-decl':0, 'input-xml':1})
+   parser = make_parser()
+   curHandler = SGMLToMalletHandler()
+   #parser.setContentHandler(curHandler)
+   xml.sax.parseString("<report>" + xhtml + "</report>", curHandler)
+   mallet = curHandler.mallet
+#   print "result is " + mallet
+   return mallet
+
+#path= HIDELIB + "sgml2mallet-stdin.pl"
+#print >> sys.stderr, "[" + path + "]"
+#add features to the mallet file
+#proc = subprocess.Popen("perl " + path,
+#		shell=True,
+#		stdin=subprocess.PIPE,
+#		stdout=subprocess.PIPE,
+#		stderr=subprocess.PIPE,
+#		)
+#stdout_value, stderr_value = proc.communicate(str(sgml))
+#print str(stderr_value)
+#print "SGMLToMallet RETURNING " + stdout_value
+#return stdout_value
+
 def MalletToSGML ( mallet ):
+   sgml = ''
+
+   fvs = re.split('\n' , mallet)
+   currentTag = "O"
+
+   for fv in fvs:
+      #get the term from the fvs
+      fv = fv.strip()
+      if fv == '':
+         continue
+      features = re.split('\\s', fv)
+      go = False
+      term = ''
+      i = 2
+      while ( not go ):
+         token = features[len(features)-i]
+         m = re.match('^TERM_(.*)$', token)
+         if m:
+            term = m.group(1)
+            go = True
+         else:
+            if ( i > 2 ):
+               print "ERROR: we don't have a term yet [" + token + "]"
+               print str(features)
+            i += 1
+      #print features
+      label = features[len(features)-1]
+      #print "writing out " + term + " with label " + label
+      term = urllib.unquote(term)
+      m = re.match('^B-(.*)$', label)
+      if m:
+         l = m.group(1)
+         currentTag = l
+         sgml += '<' + currentTag + '>'
+
+      if currentTag != 'O' and label == 'O':
+         sgml += '</' + currentTag + '>'
+         currentTag = 'O'
+
+      sgml += term
+
+   return sgml
 	#for now we use the old perl code to this conversion and return the result
 	#print "MalletToSGML: " + mallet
-	path = HIDELIB + "mallet2sgml-stdin.pl"
-	print >> sys.stderr, "[" + path + "]"
+#	path = HIDELIB + "mallet2sgml-stdin.pl"
+#	print >> sys.stderr, "[" + path + "]"
 	#add features to the mallet file
-	proc = subprocess.Popen("perl " + path,
-			shell=True,
-			stdin=subprocess.PIPE,
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE,
-			)
-	stdout_value, stderr_value = proc.communicate(mallet)
-	#print >> sys.stderr, str(stderr_value)
-	#print >> sys.stderr, "RETURNING " + str(stdout_value)
-	return str(stdout_value)
+#	proc = subprocess.Popen("perl " + path,
+#			shell=True,
+#			stdin=subprocess.PIPE,
+#			stdout=subprocess.PIPE,
+#			stderr=subprocess.PIPE,
+#			)
+#	stdout_value, stderr_value = proc.communicate(mallet)
+#	#print >> sys.stderr, str(stderr_value)
+#	#print >> sys.stderr, "RETURNING " + str(stdout_value)
+#	return str(stdout_value)
 	
 
 	
@@ -238,6 +243,119 @@ def buildTemplate( sgml, tagstoa ):
 
 
 def addFeatures( mallet ):
+   """Generates dictionary, history , and regular expression based features for input."""
+
+   history = []
+   historySize = 4
+
+   mallet = mallet.strip()
+   fvs = mallet.split("\n")
+   for i in range(len(fvs)):
+      fvs[i] = fvs[i].strip()
+      if fvs[i] == '':
+         print "warning we have an empty FV"
+         continue
+      features = re.split('\\s', fvs[i])
+      token = features[len(features)-2]
+      m = re.match('TERM_(.*)', token)
+      term = m.group(1)
+      cterm = urllib.unquote(term)
+      foundfeatures = regexMatchFeatures( cterm )
+      affixfeatures = affixFeatures( cterm )
+      foundfeatures.extend(affixfeatures)
+      for j in range(0, historySize):
+         if j >= len(history):
+            break
+         foundfeatures.append( "PREV" + str(j) + "_" + history[j] )
+         fvs[i-j-1] = "NEXT" + str(j) + "_" + term + " " + fvs[i-j-1]
+
+      history.append( term )
+      if ( len(history) > historySize ):
+         history = history[1:(len(history)-1)]
+
+      fvs[i] = " ".join(foundfeatures) + " " + fvs[i]
+      in_dict = checkDictionaries( term )
+      if in_dict != '':
+         #print "FOUND that [" + term + "] is in " + in_dict
+         fvs[i] = in_dict + " " + fvs[i]
+   features = "\n".join(fvs) #remake the string from the fvs
+   features = features.strip()
+   return features
+
+def affixFeatures( term ):
+   alength = 3
+   if (len(term) < alength):
+      alength = len(term)-1
+   features = []
+   if len(term) > 1:
+      for i in range(1, alength+1):
+         prefix = term[0:i]
+         suffix = term[(len(term)-i):(len(term))]
+         features.append("PRE" + str(i) + "_" + prefix)
+         features.append("SUF" + str(i) + "_" + suffix)
+   return features
+
+def regexMatchFeatures( term ):
+   features = []
+#   print "checking features for [" + term + "]"
+   if re.match( '^[A-Za-z]$', term ):
+      features.append( 'ALPHA' )
+   if re.match( '^[A-Z].*$', term):
+      features.append( 'INITCAPS' )
+   if re.match( '^[A-Z][a-z].*$', term ):
+      features.append( 'UPPER-LOWER')
+   if re.match( '^[A-Z]+$', term ):
+      features.append( 'ALLCAPS' )
+   if re.match( '^[A-Z][a-z]+[A-Z][A-Za-z]*$', term ):
+      features.append( 'MIXEDCAPS' )
+   if re.match( '^[A-Za-z]$', term ):
+      features.append('SINGLECHAR')
+   if re.match( '^[0-9]$', term):
+      features.append('SINGLEDIGIT')
+   if re.match( '^[0-9][0-9]$', term):
+      features.append('DOUBLEDIGIT')
+   if re.match( '^[0-9][0-9][0-9]$', term):
+      features.append('TRIPLEDIGIT')
+   if re.match( '^[0-9][0-9][0-9][0-9]$', term):
+      features.append('QUADDIGIT')
+   if re.match( '^[0-9,]+$', term):
+      if not re.match( '^,$', term ):
+         features.append('NUMBER')
+   if re.search( '[0-9]', term):
+      features.append('HASDIGIT')
+   if re.match( '^.*[0-9].*[A-Za-z].*$', term):
+      features.append("ALPHANUMERIC")
+   if re.match( '^.*[A-Za-z].*[0-9].*$', term):
+      features.append("ALPHANUMERIC")
+   if re.match( '^[0-9]+[A-Za-z]$', term ):
+      features.append("NUMBERS_LETTERS")
+   if re.match( '^[A-Za-z]+[0-9]+$', term ):
+      features.append( "LETTERS_NUMBERS" )
+   if re.search ( '-', term ):
+      features.append( "HASDASH" )
+   if re.search ( "'", term ):
+      features.append( "HASQUOTE" )
+   if re.search ( "/", term ):
+      features.append( "HASSLASH" )
+   if re.match( r"[`~!@#$%\^&*()\-=_+\[\]{}|;':\",./<>?]+$", term ):
+      features.append( "ISPUNCT" )
+   if re.match( r"(-|\+)?[0-9,]+(\.[0-9]*)?%?$", term ):
+      features.append( "REALNUMBER" )
+   if re.match( '^-.*$', term ):
+      features.append ( "STARTMINUS" )
+   if re.match( r"^\+.*$", term ):
+      features.append ( "STARTPLUS" )
+   if re.match( r"^.*%$", term):
+      features.append ( "ENDPERCENT" )
+   if re.match( r"^[IVXDLCM]+$", term ):
+      features.append( "ROMAN" )
+   if re.match( r"^\s+$", term):
+      features.append( "ISSPACE" )
+#   print "\n" + str(features)
+   return features
+
+
+def addFeaturesPerl( mallet ):
    """Currently calls perl to generate most regular expression features.
       The dictionary features are added in the python code.
       The dictionary features are currently case-insensitive."""
@@ -274,17 +392,13 @@ def addFeatures( mallet ):
    for i in range(len(fvs)):
       fv = fvs[i]
       fs = re.split('\s+', fv)
-      for f in fs:
-         #this is down to the actual feature level
-	 # find the TERM_ -> check dictionary -> append to fv and break
-	 m = re.match('TERM_(.*)', f)
-	 if m:
-	    term = m.group(1)
-	    in_dict = checkDictionaries( term )
-	    if in_dict != '':
-	       #print "FOUND that [" + term + "] is in " + in_dict
-	       fvs[i] = in_dict + " " + fv
-	       break
+      token = fs[len(fs)-2]
+      m = re.match('TERM_(.*)', f)
+      term = m.group(1)
+      in_dict = checkDictionaries( term )
+      if in_dict != '':
+         #print "FOUND that [" + term + "] is in " + in_dict
+         fvs[i] = in_dict + " " + fv
    features = "\n".join(fvs) #remake the string from the fvs
    return features
 
@@ -311,19 +425,24 @@ def trainModel( modelfile, mallet ):
    execme = 'java ' + javaargs + " EmoryCRF --fully-connected false --train true --model-file " + modelfile + " " + tempfile.name + " 2>" + modelfile + ".log"
 
    print >> sys.stderr, "executing " + execme
+   prevtime = time.time()
 
-   #open up EmoryCRF
    proc = subprocess.Popen( execme,
                shell=True,
                stdout=subprocess.PIPE,
             #   stderr=subprocess.PIPE,
                )
-   #read output from EmoryCRF
    stdout_value, stderr_value = proc.communicate()
    os.unlink(tempfile.name)
+   currtime = time.time()
+   passedtime = currtime - prevtime
+   f = open(modelfile + ".log", 'a')
+   f.write("\n\n" + str(passedtime))
+   f.close()
 
 
 def testModel (outfile, modelfile, mallet ):
+   prevtime = time.time()
    tempfile = NamedTemporaryFile(delete=False)
    tempfile.write(mallet)
    tempfile.close()
@@ -335,8 +454,11 @@ def testModel (outfile, modelfile, mallet ):
    f.write(results)
    f.close()
    print >> sys.stderr, "done writing results";
+   currtime = time.time()
+   passedtime = currtime - prevtime
+   print "elapsed test time = " + str(passedtime)
 
-   return 1
+   return results
    
 
 
@@ -345,7 +467,6 @@ def labelMallet( modelfile, mallet ):
    tempfile.write(mallet)
    tempfile.close()
 
-   #setup the external call to EmoryCRF
    #TODO update this to allow the user to specify the CRF to use.
    javaclasspath = EMORYCRFLIB + "emorycrf" + ":" + EMORYCRFLIB + "mallet/class/:" + EMORYCRFLIB + "mallet/lib/mallet-deps.jar"
    javaargs = "-Xmx" + MAXMEM + " -cp \"" + javaclasspath + "\""
@@ -353,14 +474,12 @@ def labelMallet( modelfile, mallet ):
 
    print >> sys.stderr, "executing " + execme
 
-   #open up EmoryCRF
    proc = subprocess.Popen( execme,
                shell=True,
                stdout=subprocess.PIPE,
                stderr=subprocess.PIPE,
                )
 
-   #read output from EmoryCRF
    stdout_value, stderr_value = proc.communicate()
    #print >> sys.stderr, stderr_value
 
@@ -390,6 +509,27 @@ def labelMallet( modelfile, mallet ):
 
    print "returning results"
    return resultsmallet
+
+def getCRFNamesFromDir( dir ):
+   context = []
+   for fileName in os.listdir ( dir ):
+      if re.search('\.crf$', fileName):
+         context.append(fileName)
+   return context
+
+def getFVNamesFromDir( dir ):
+   context = []
+   for fileName in os.listdir ( dir ):
+      if re.search('\.features$', fileName):
+         context.append(fileName)
+   return context
+
+def getResultNamesFromDir( dir ):
+   context = []
+   for fileName in os.listdir ( dir ):
+      if re.search('\.results$', fileName):
+         context.append(fileName)
+   return context
 
 def getTags ( xml ):
 #extracts all the tag names in the xml document
