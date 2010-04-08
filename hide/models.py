19b511112f2829af677144e5fc80f629b9bc24c5
pebbler <pebbler@2da577ee-07a7-11df-a26f-8b716316a5bc>
2010-04-08 18:59:45 +0000

    Moved Object class definition into models module

--- hide/models.py
+++ hide/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from couchdbkit import *
+
+class Object(Document):
+   author = StringProperty()
+   title = StringProperty()
+   text = StringProperty()
+   tags = StringProperty()
+   id = StringProperty()
 
 # Create your models here.
